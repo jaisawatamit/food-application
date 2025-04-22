0d9097c12b56771b38d1f1efd0d10954250c1e2a
jaisawatamit <jaisawatamit@gmail.com>
2025-04-22 10:48:31 +0530

    Fix cartSlice import case sensitivity

--- food-web/src/components/Card2.jsx
+++ food-web/src/components/Card2.jsx
@@ -14,7 +14,7 @@ const Card2 = ({name, price, id, qty, image}) => {
           <img src={image} alt="" className='object-cover' />
         </div>
         <div className='w-[40%] h-full flex flex-col gap-5'>
-          <div className='text-[20px] text-gray-600 font-semibold'>{name}</div>
+          <div className='text-[17px] text-gray-600 font-semibold'>{name}</div>
           <div className='w-[110px] h-[50px] flex justify-center items-center rounded-lg shadow-lg font-semibold border-2 border-green-400 text-xl overflow-hidden'>
             <button className='w-[30%] hover:bg-gray-200 text-green-500  h-full bg-white flex justify-center items-center' onClick={()=> {qty>1? dispatch(decItem(id)): 1}}>-</button>
             <span className='w-[30%] text-green-500  h-full bg-slate-200 flex justify-center items-center'>{qty}</span>
